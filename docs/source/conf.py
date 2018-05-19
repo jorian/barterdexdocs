f90913655a1c7d2ff4c7c35da101c7a44a33c872
gcharang <21151592+gcharang@users.noreply.github.com>
2018-05-19 10:52:56 +0530

    updated conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -92,7 +92,7 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {'canonical_url': 'http://barterdex.readthedocs.io/en/latest/',
+html_theme_options = {'canonical_url': 'http://barterdexdocs.readthedocs.io/en/latest/',
    # 'analytics_id': '',
     #'logo_only': False,
    # 'display_version': True,
